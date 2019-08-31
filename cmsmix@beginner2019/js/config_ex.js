be4fb5095c7798a1716aa4080626fe97c5d59e39
WebbingStudio <mail@webbingstudio.com>
2019-08-31 23:23:49 +0900

    ハンズオン終了時点のテーマ一式をpush

---
+++ cmsmix@beginner2019/js/config_ex.js
@@ -0,0 +1,16 @@
+ACMS.Ready(function() {
+
+
+//  ACMS.Config.modalVideoConfig.youtube = {
+//    controls: 0
+//  };
+
+//  ACMS.Config.modalVideoConfig.ratio = '4:3';
+
+//  ACMS.Config.SmartPhotoConfig = {
+//    arrows: false,
+//    nav: false,
+//  }
+
+
+});
